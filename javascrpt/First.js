3aae525966f2a21112891e10a0954f37615f9a79
Husnain <Husnain.tayab@hotmail.com>
2026-03-25 00:31:22 +0500

    Java script

---
+++ javascrpt/First.js
@@ -0,0 +1,16 @@
+console.log("Hello, World!");
+let age = 25;
+let name = "Husnain";
+let isStudent = true;
+let bigint = 12345678901234567890n;
+let symbol = Symbol("unique");
+let undefinedValue;
+let nullValue = null;
+
+console.log(age);       
+console.log(name);      
+console.log(isStudent); 
+console.log(bigint);    
+console.log(symbol);    
+console.log(undefinedValue);
+console.log(nullValue);
