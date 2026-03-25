d49f13749fb9a4072f125edb4357e98cfe962c44
Husnain <Husnain.tayab@hotmail.com>
2026-03-25 22:20:39 +0500

    Javascript with object

--- javascrpt/object.js
+++ javascrpt/object.js
@@ -1,9 +1,13 @@
 const student = {
+
     name: "Husnain",
     age: 22,
     city: "Karachi",
     country: "Pakistan",
     hobbies: ["Coding", "Gaming", "Traveling"],
     isStudent: true,
+    greet: function() {
+        return "Hello, my name is " + this.name + " and I am from " + this.city + ".";
+    }
 };
-console.log(student);
+console.log(student.greet());
