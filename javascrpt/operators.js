7b01704d84222ce7e462c36002c793779f26ac6f
Husnain <Husnain.tayab@hotmail.com>
2026-03-25 22:19:30 +0500

    Javascript operators

--- javascrpt/operators.js
+++ javascrpt/operators.js
@@ -39,6 +39,7 @@ console.log("Division Assignment:", c);
 c %= 3; // c = c % 3
 console.log("Modulus Assignment:", c);
 
+
 // Increment and Decrement Operators
 let d = 7;
 console.log("Post-increment:", d++); // Outputs 7, then d becomes 8
