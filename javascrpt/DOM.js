faf58f3100f4ae358c27f8978ab0681f0cdfb06d
Husnain <Husnain.tayab@hotmail.com>
2026-04-02 23:09:56 +0500

    Document object model in javascript

--- javascrpt/DOM.js
+++ javascrpt/DOM.js
@@ -1,3 +1,12 @@
 let heading = document.getElementById("heading")
 console.dir(heading)
 console.log(heading)
+
+let description = document.getElementById("description")
+console.dir(description)
+console.log(description)
+
+
+let changeText = document.getElementById("changeText")
+console.dir(changeText)
+console.log(changeText)
